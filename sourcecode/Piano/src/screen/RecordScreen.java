6e7bf000a88c5d9a355a568be4f79b4dcf9358a2
giangNT1609 <giang.nt194751@sis.hust.edu.vn>
2022-07-02 22:02:42 +0700

    add all

--- sourcecode/Piano/src/screen/RecordScreen.java
+++ sourcecode/Piano/src/screen/RecordScreen.java
@@ -13,10 +13,6 @@ public class RecordScreen {
 	public RecordScreen() {
 		initialize();
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 771, 390);
